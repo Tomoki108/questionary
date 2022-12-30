36982c2c299e7bff7e0caa4d380badd4979a52f5
Tomoki108 <shinonome81@gmail.com>
2022-12-30 23:15:09 +0900

    create sfcs as outline

---
+++ src/components/containers/FormContainer.vue
@@ -0,0 +1,26 @@
+<template>
+      <li v-for="keyword in keywords">
+        <Keyword/>
+        <Impression/>
+        <Impression/>
+        <Impression/>
+    </li>
+    <SubmitButton/>
+</template>
+
+<script setup lang="ts">
+import Keyword from "../presentationals/keyword.vue";
+import Impression from "../presentationals/Impression.vue";
+import SubmitButton from "../presentationals/SubmitButton.vue";
+
+export interface Props {
+    keywords: string[]
+}
+
+const props = withDefaults(
+    defineProps<Props>(),
+    { keywords: () => ['Dog', 'Cat', 'Human', 'Wombat'] }
+);
+
+</script>
+
